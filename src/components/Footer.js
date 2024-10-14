28fac5b322bbe7d23381b9d4580903cf5c9fd5bd
pidanet <146123013+pidanet@users.noreply.github.com>
2024-10-14 12:44:47 +0200

    Delete src directory

--- src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <footer className="footer">Petr Vitvar 2024</footer>;
-};
-
-export default Footer;
--- src/pages/categories.js
+++
@@ -1,3 +0,0 @@
-const categories = ["První", "Druhý", "Třetí"];
-
-export default categories;
